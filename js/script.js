f7e2969a872d2a825d504a5213131481c60b6d45
Facundo <facurtprn@gmail.com>
2022-03-09 19:48:15 -0300

    se agregan funciones

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// variables
+
 let saldo = 23500;
 let saldoIngresado =0;
 let depositoSaldo =0;
@@ -6,21 +8,45 @@ let nuevoSaldo = (saldo+depositoSaldo);
 let opcion = prompt(
   "Elegí una opción: 1- Saldos. 2 - Retiro de dinero. 3 - Depositos. Presioná X para finalizar"
 );
+// fin variables
 
+// ------------------------------------------
+
+// funciones
+  const retiro = function (params) {
+
+    saldoIngresado = parseInt(prompt('Ingrese el monto a retirar')); if(saldoIngresado>saldo) {
+      alert('Saldo insuficiente')   
+      
+  }else {
+
+    saldo = saldo - saldoIngresado;
+
+    alert('Retiro de dinero exitoso, su nuevo saldo es $' + saldo)
+
+  }
+    
+  }
+// fin funciones
+
+
+// codigo
 while (opcion != "X") {
   switch (opcion) {
     case '1':
         alert('Tu saldo es $'+ saldo)
       break;
     case '2':
-        saldoIngresado = parseInt(prompt('Ingrese el monto a retirar')); if(saldoIngresado>saldo) {
-            alert('Saldo insuficiente')   
-        }else {
-          saldo = saldo - saldoIngresado;
+        // saldoIngresado = parseInt(prompt('Ingrese el monto a retirar')); if(saldoIngresado>saldo) {
+        //     alert('Saldo insuficiente')   
+        // }else {
+        //   saldo = saldo - saldoIngresado;
 
-          alert('Retiro de dinero exitoso, su nuevo saldo es $' + saldo)
+        //   alert('Retiro de dinero exitoso, su nuevo saldo es $' + saldo)
 
-        }
+        // }
+
+        retiro();
       break;
 
 
