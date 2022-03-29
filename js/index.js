e752a538979c3e7adb6b47f9a19c25ff91d45a47
Facundo <facurtprn@gmail.com>
2022-03-29 12:38:19 -0300

    se hace adicion de eventos en rama de prueba y se crea archivo tienda.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,68 @@
 let saldo = 23500;
 
 
+// intentamos agregar eventos a la pagina por cada boton
+
+// boton saldo
+
+let botonConsulta = document.getElementById("botonSaldo")
+botonConsulta.addEventListener("click", accionBotonSaldo)
+
+
+function accionBotonSaldo ( ){
+  alert("Tu saldo es $" + saldo);
+};
+// fin boton saldo
+/* ******************************************************* */ 
+/* boton retiro dinero*/
+
+let botonExtraccion = document.getElementById("botonRetiro")
+botonExtraccion.addEventListener("click", accionRetiro)
+
+function accionRetiro() {
+  let monto = parseFloat(prompt("Ingrese el monto a retirar"));
+  if (monto > saldo) {
+    alert("Saldo insuficiente");
+  } else if (monto <= 0) {
+    alert("Ingrese un monto válido");
+  } else {
+    saldo = retirarDinero(saldo, monto);
+    alert("Retiro de dinero exitoso, su nuevo saldo es $" + saldo);
+  }
+}
+
+// FIN BOTON RETIRO
+/* ******************************************************* */ 
+// BOTON INGRESO DINERO 
+let botonAdicionarDinero = document.getElementById("botonDeposito")
+botonAdicionarDinero.addEventListener("click", accionDeposito)
+
+// funcion accionDeposito
+
+function accionDeposito (){
+  let depositoSaldo = parseInt(prompt("Ingrese el monto a depositar"));
+  if (depositoSaldo > 0) {
+    saldo = depositarDinero(saldo, depositoSaldo);
+    alert(
+      "Usted deposito $" +
+        depositoSaldo +
+        "." +
+        " Su nuevo saldo es $" +
+        saldo
+    );
+  } else {
+    alert("Ingrese un monto valido");
+  }
+}
+
+// FIN BOTON INGRESO 
+/* ******************************************************* */ 
+//  BOTON TRANSFERENCIA
+
+// IMPORTANTE, TRATAR DE SACAR ESE BUG
+
+let botonTransferir = document.getElementById("botonTransferencia")
+botonTransferir.addEventListener("click", transferir)
 // Creamos array y agregamos objetos en el
 
 const listaDestinatario = [];
@@ -126,151 +188,59 @@ let opcion = prompt(
   "Elegí una opción: 1- Saldos. 2 - Retiro de dinero. 3 - Depositos. 4 - Transferencias. - Presioná X para finalizar"
 );
 
-while (opcion.toUpperCase() != "X") {
-  switch (opcion) {
-    case "1":
-      // Informar saldo
-      alert("Tu saldo es $" + saldo);
-      break;
-    case "2":
-      // Retirar dinero
-      // Number() hace exactamente lo mismo que parseInt(). Es solo otra manera de escribirlo
-      let monto = Number(prompt("Ingrese el monto a retirar"));
-      // También verificar que el ingreso sea mayor a 0
-      if (monto > saldo) {
-        alert("Saldo insuficiente");
-      } else if (monto <= 0) {
-        alert("Ingrese un monto válido");
-      } else {
-        saldo = retirarDinero(saldo, monto);
-        alert("Retiro de dinero exitoso, su nuevo saldo es $" + saldo);
-      }
-      break;
-    case "3":
-      // Depositar dinero
-      let depositoSaldo = parseInt(prompt("Ingrese el monto a depositar"));
-      if (depositoSaldo > 0) {
-        saldo = depositarDinero(saldo, depositoSaldo);
-        alert(
-          "Usted deposito $" +
-            depositoSaldo +
-            "." +
-            " Su nuevo saldo es $" +
-            saldo
-        );
-      } else {
-        alert("Ingrese un monto valido");
-      }
+// while (opcion.toUpperCase() != "X") {
+//   switch (opcion) {
+//     case "1":
+//       // Informar saldo
+//       alert("Tu saldo es $" + saldo);
+//       break;
+//     case "2":
+//       // Retirar dinero
+//       // Number() hace exactamente lo mismo que parseInt(). Es solo otra manera de escribirlo
+//       let monto = Number(prompt("Ingrese el monto a retirar"));
+//       // También verificar que el ingreso sea mayor a 0
+//       if (monto > saldo) {
+//         alert("Saldo insuficiente");
+//       } else if (monto <= 0) {
+//         alert("Ingrese un monto válido");
+//       } else {
+//         saldo = retirarDinero(saldo, monto);
+//         alert("Retiro de dinero exitoso, su nuevo saldo es $" + saldo);
+//       }
+//       break;
+//     case "3":
+//       // Depositar dinero
+//       let depositoSaldo = parseInt(prompt("Ingrese el monto a depositar"));
+//       if (depositoSaldo > 0) {
+//         saldo = depositarDinero(saldo, depositoSaldo);
+//         alert(
+//           "Usted deposito $" +
+//             depositoSaldo +
+//             "." +
+//             " Su nuevo saldo es $" +
+//             saldo
+//         );
+//       } else {
+//         alert("Ingrese un monto valido");
+//       }
 
-      break;
+//       break;
 
-    case "4":
-      alert("Elija una opcion para transferir");
-      transferir();
+//     case "4":
+//       alert("Elija una opcion para transferir");
+//       transferir();
 
       
 
-      break;
+//       break;
 
-    default:
-      alert("Opcion Incorrecta");
-      break;
-  }
+//     default:
+//       alert("Opcion Incorrecta");
+//       break;
+//   }
 
-  opcion = prompt(
-    "Elegí una opción: 1- Saldos. 2 - Retiro de dinero. 3 - Depositos. 4 - Transferencias. - Presioná X para finalizar"
-  );
-}
+//   opcion = prompt(
+//     "Elegí una opción: 1- Saldos. 2 - Retiro de dinero. 3 - Depositos. 4 - Transferencias. - Presioná X para finalizar"
+//   );
+// }
 
-// CODIGO Y ARRAY DE TIENDA
-
-let productos = [
-  {   
-      id:1,
-      nombre:"Cocina",
-      precio: "$50000",
-      puntos: 9600,
-      imagen:"/images/cocina.jpg",
-  },
-
-  {   
-      id:2,
-      nombre:"Heladera",
-      precio: "$168999",
-      puntos: 10400,
-      imagen:"/images/heladera.jpg",
-  },
-
-  {   
-      id:3,
-      nombre:"Aire Acondicionado",
-      precio: "$189999",
-      puntos: 20000,
-      imagen:"/images/aire-acondicionado.jpg",
-  },
-
-  {   
-      id:4,
-      nombre:"Lavarropas",
-      precio: "$125499",
-      puntos: 10300,
-      imagen:"/images/lavarropas-1.png",
-  },
-
-  {   
-      id:5,
-      nombre:"Microondas",
-      precio: "$32500",
-      puntos: 5800 ,
-      imagen:"/images/microondas.jpg",
-  },
-
-  {   
-      id:6,
-      nombre:"Televisor",
-      precio: "$119899",
-      puntos: 10200,
-      imagen:"/images/televisor.webp",
-  },
-
-  {   
-    id:7,
-    nombre:"Lavavajillas",
-    precio: "$100999",
-    puntos: 10600,
-    imagen:"/images/lavavajilla.jpg",
-},
-
-{   
-  id:8,
-  nombre:"Cafetera 3 en 1",
-  precio: "$28799",
-  puntos: 6700,
-  imagen:"/images/cafetera.jpg",
-},
-];
-
-// HACEMOS UN FILTRO PARA QUE NOS DEVUELVA UN ARRAY CON LOS ELEMENTOS QUE REQUIERAN MAS DE 10000 PUNTOS 
-let productosPuntos = productos.filter((elemento)=> elemento.puntos > "10000")
-
-console.table(productosPuntos);
-
-// CREAMOS LA CARD UTILIZANDO LOS ELEMENTOS DEL ARRAY Y CON CLASES DE BOOT, COMO HIZO VANI EN EL AFTER
-const contenedor = document.getElementById("container");
-productos.forEach((producto)=>{
-  let card = document.createElement('div');
-  card.classList.add("card","col-sm-12","col-lg-3");
-  card.innerHTML =  `<img src=${producto.imagen} class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${producto.nombre}</h5>
-    <p class="card-text">${producto.precio}</p>
-    <p class="card-text">${producto.puntos}</p>
-    <a href="#" class="btn btn-primary">Comprar</a>
-    <a href="#" class="btn btn-primary">Canjear</a>
-  </div>`;
-  
-  contenedor.appendChild(card);
-}
-
-
-);
